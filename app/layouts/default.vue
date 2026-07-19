8053220bec1f5d3526a352532cdf6e73eb5ca205
Seidy Alanis <seidyalanis@gmail.com>
2026-07-18 21:21:24 -0600

    Creación de layouts para la imagen de fondo estándar en la páginas.

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="relative min-h-screen bg-cover bg-center"
+    style="background-image: url('/fondo-mundial.jpg')"
+  >
+    <div class="absolute inset-0 bg-black/25" />
+    <div class="relative z-10">
+      <slot />
+    </div>
+  </div>
+</template>
